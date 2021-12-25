347d0c9ebdd6e8f43084150b8285bc953ff6d6ac
Maurício Ricardo <mauriciod37@gmail.com>
2021-12-25 16:32:29 -0300

    rentx-ignite-modulo03

---
+++ src/modules/cars/repositories/ISpecificationsRepository.ts
@@ -0,0 +1,11 @@
+interface ICreateSpecificationDTO {
+  name: string;
+  description: string;
+}
+
+interface ISpecificationsRepository {
+  create({ description, name }: ICreateSpecificationDTO): Promise<void>;
+  findByName(name: string): Promise<Specification>;
+}
+
+export { ISpecificationsRepository };
